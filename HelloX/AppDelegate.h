0553888f43ae06748d7d4ad5f5190172762e16a7
Frank Ortiz <ortiz_frank@hotmail.com>
2015-02-17 19:28:13 -0500

    Initial Commit

---
+++ HelloX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloX
+//
+//  Created by Frank Ortiz on 2/17/15.
+//  Copyright (c) 2015 Wyncode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloX/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloX
+//
+//  Created by Frank Ortiz on 2/17/15.
+//  Copyright (c) 2015 Wyncode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
